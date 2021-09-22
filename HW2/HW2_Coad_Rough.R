e5a7043db8f11b3e027bbb42159469d4f07f0b60
Jason Pekos <Jason.Pekos@gmail.com>
2021-09-22 18:30:58 -0400

    added code and first plot

---
+++ HW2/HW2_Coad_Rough.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(mgcv)
+
+#read in data
+df = readr::read_csv("https://mac-theobio.github.io/DataViz/data/vaccine_data_online.csv")
+
+#get rid of the non-measles data, and useless columns which are only used for other data (all FALSE here)
+df_minimal = filter(df, disease == "Measles")[1:5]
+
+#check to make sure everything is working (commented out)
+#print(as_tibble(df_minimal), n = 70)
+
+#make first plot
+ggplot(data = df_minimal, aes(x = year, y = cases)) +
+  geom_smooth() + 
+  geom_point(shape = 4) +
+  geom_vline(xintercept = filter(df_minimal,
+                                 vaccine != FALSE)$year,
+             linetype = "dashed")+
+  labs(y = "cases", x = "year")
+
+# The issue is that the later points drag down the loess fit 
+# and make it look like the trend was decereasing before the vaccine
+# which sends .. probably a bad (&wrong) message. I plotted the actual points
+# which helps a little, but I still think this is bad, so I'm decreasing the loess span
+
+
+ggplot(data = df_minimal, aes(x = year, y = cases)) +
+  geom_smooth(span = 0.09) + #method = mgcv::gam()
+  geom_point(shape = 4) +
+  geom_vline(xintercept = filter(df_minimal,
+                                 vaccine != FALSE)$year,
+             linetype = "dashed")+
+  labs(y = "cases", x = "year")
+
+
+
+##Discontinuity Attempt 
+#specify the breakpoint year because I was running into issue with nested verbs
+
+bp = filter(df_minimal,vaccine != FALSE)$year
+
+#credit to here: https://rstudio-pubs-static.s3.amazonaws.com/116317_e6922e81e72e4e3f83995485ce686c14.html#/5
+#for mutate + iflelse troubleshooting help
+#nvm in the end it was a bracket issue :-/
+#credit to https://stackoverflow.com/a/61702104/12369476
+#for vline label also 
+
+df_minimal %>% mutate(post_date = ifelse(year > bp,"yes","no")) -> df_minimal
+
+ggplot(data = df_minimal, aes(x = year, y = cases, color = post_date)) +
+  geom_smooth(span = 0.5) + #method = mgcv::gam()
+  geom_point(shape = 4) +
+  geom_vline(xintercept = bp,
+             linetype = "dashed") +
+  labs(y = "cases", x = "year", color = "Vaccine \nAvailable?") +
+  ggtitle("Recorded Measles Cases Before and After Introduction of Vaccine") +
+  annotate(x=bp,y=+Inf,label=" < Vaccine Introduced",vjust=3, hjust = -0.11, geom="label") 
+
+
+
+
+
+
--- HW2/hw2.md
+++ HW2/hw2.md
@@ -1,6 +1,25 @@
 # A2 —  Jason Pekos
 
-*Asg: write a short statement (a few sentences) that explains what question you think the graphic is trying to answer, or what pattern it’s trying to display*
+---
+
+*Q1*: write a short statement (a few sentences) that explains what question you think the graphic is trying to answer, or what pattern it’s trying to display*
+
+
+
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/JasonPekos/Stat744/main/HW2/you_graph_original.jpg" width="350" title="initial graph from https://www.science.org/news/2017/04/here-s-visual-proof-why-vaccines-do-more-good-harm">
+
+</p>
+
+
+
+**A:**  The purpose of the graph is to demonstrate the efficacy of the Measles vaccine by plotting the time series of measles cases each year before and after the introduction of the Measles vaccine. The pattern is a clear decrease in the prevalence of the disease — represented by the area of the blue circles — in the years following the vaccine rollout. 
+
+
+---
+
+*Q2*: based on these data, create 2 ggplots that display the data in a different way from the original; use the 2 plots to illustrate tradeoffs between different graphical principles. (If you’re feeling truly uncreative, one of your plots can replicate the original graphical design.) You do not need to worry about the dynamic-graphics aspect or the historical events shown in the original display
 
 
 
